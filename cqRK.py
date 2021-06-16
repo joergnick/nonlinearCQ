Bugfix regarding gradient.

cqRK.py
@@ -35,17 +35,26 @@ class CQModel:
 		import math
 		dof = len(rhs)
 		m = len(W0)
+		print("X0",x0)
+		for stageInd in range(m):
+			for j in range(dof):
+				if np.abs(x0[j,stageInd])<10**(-3):
+					x0[j,stageInd] = 10**(-3)
+		print("X0 repaired: ", x0)
 		Tinv = np.linalg.inv(Tdiag)
 		grada = np.zeros((m*dof,m*dof))
 		rhsLong = 1j*np.zeros(m*dof)
-		taugrad = 10**(-8)
+		taugrad = 10**(-5)
 		idMat = np.identity(dof)
-
-		for stageIndx,stageIndy in zip(range(m),range(m)):
-			for i,j in zip(range(dof),range(dof)):
-				grada[stageIndy*dof+i,stageIndx*dof+j] = (self.nonlinearity(x0[:,stageIndx]+taugrad*idMat[:,i])[j]-self.nonlinearity(x0[:,stageIndx]-taugrad*idMat[:,i])[j])/(2*taugrad)
-				if math.isnan(grada[stageIndx*dof+i,stageIndy*dof+j]):
-					grada[stageIndx*dof+j,stageIndy*dof+j]=0
+		for stageIndx in range(m):
+			for stageIndy in range(m):
+				for i in range(dof):
+					for j in range(dof):
+						grada[stageIndy*dof+i,stageIndx*dof+j] = (self.nonlinearity(x0[:,stageIndx]+taugrad*idMat[:,i])[j]-self.nonlinearity(x0[:,stageIndx]-taugrad*idMat[:,i])[j])/(2*taugrad)
+						if math.isnan(grada[stageIndy*dof+i,stageIndx*dof+j]):
+#							print("NAN occuring in gradient, substituted via 0. ")
+							grada[stageIndy*dof+i,stageIndx*dof+j]=0
+		print(grada)
 		#rhs = W0*x0+self.nonlinearity(x0)-rhs
 		stageRHS = x0+1j*np.zeros((dof,m))
 		## Calculating right-hand side
@@ -73,6 +82,7 @@ class CQModel:
 		NewtonLambda = lambda x: NewtonFunc(x)
 		NewtonOperator = LinearOperator((m*dof,m*dof),NewtonLambda)
 		dxlong,info = gmres(NewtonOperator,rhsLong,tol=1e-8)
+		
 		if info != 0:
 			print("GMRES Info not zero, Info: ", info)
 		dx = 1j*np.zeros((dof,m))
@@ -138,8 +148,10 @@ class CQModel:
 					extr[:,i] = self.extrapol(sol[:,:j*m+i:m],m+1)
 				else:
 					extr[:,i] = np.zeros(dof)
-
 			sol[:,j*m+1:(j+1)*m+1] = np.real(self.newtonsolver(deltaEigs,rhs[:,j*m+1:(j+1)*m+1],W0,Tdiag,extr,charMatrix0))
+			sol[:,j*m+1:(j+1)*m+1] = np.real(self.newtonsolver(deltaEigs,rhs[:,j*m+1:(j+1)*m+1],W0,Tdiag,sol[:,j*m+1:(j+1)*m+1],charMatrix0))
+			sol[:,j*m+1:(j+1)*m+1] = np.real(self.newtonsolver(deltaEigs,rhs[:,j*m+1:(j+1)*m+1],W0,Tdiag,sol[:,j*m+1:(j+1)*m+1],charMatrix0))
+			sol[:,j*m+1:(j+1)*m+1] = np.real(self.newtonsolver(deltaEigs,rhs[:,j*m+1:(j+1)*m+1],W0,Tdiag,sol[:,j*m+1:(j+1)*m+1],charMatrix0))
 			sol[:,j*m+1:(j+1)*m+1] = np.real(self.newtonsolver(deltaEigs,rhs[:,j*m+1:(j+1)*m+1],W0,Tdiag,sol[:,j*m+1:(j+1)*m+1],charMatrix0))
 			sol[:,j*m+1:(j+1)*m+1] = np.real(self.newtonsolver(deltaEigs,rhs[:,j*m+1:(j+1)*m+1],W0,Tdiag,sol[:,j*m+1:(j+1)*m+1],charMatrix0))
 			sol[:,j*m+1:(j+1)*m+1] = np.real(self.newtonsolver(deltaEigs,rhs[:,j*m+1:(j+1)*m+1],W0,Tdiag,sol[:,j*m+1:(j+1)*m+1],charMatrix0))

nonlinear1ddamping.py
@@ -1,20 +1,25 @@
 from cqRK import CQModel
+import numpy as np
 class ScatModel(CQModel):
 	def precomputing(self,s):
-		return np.array([[s,1],[1,s]])
+		return s**(-1)
 	def harmonicForward(self,s,b,precomp = None):
-		return precomp.dot(b)
+		return precomp*b
 	def righthandside(self,t,history = None):
-		return np.array([3*t**2+t**4+t**3+t**8,5*t**3+t**9+t**4])
+		return 1.0/4*t**4+t**1.5
 	def nonlinearity(self,x):
+	#	print("X IN NONLINEARITY: ",x," RESULT : ",abs(x)**(-0.5)*x)
+		val = np.abs(x)**(-0.5)*x
+		nanindizes = np.isnan(val)	
+		val[nanindizes] = 0
+		return val
 		#return 0*x
-		return np.array([x[0]**1+x[1]**2,x[0]**3+x[1]**(1)])
+		#return np.array([x[0]**1+x[1]**2,x[0]**3+x[1]**(1)])
 	
 model = ScatModel()
 T = 1
-Am = 4
+Am = 1
 m=3
-import numpy as np
 taus = np.zeros(Am)
 err = np.zeros(Am)
 Ns  = np.zeros(Am)
@@ -32,19 +37,19 @@ for j in range(Am):
 	#ex_sol = 4*3*tt**2
 	#method = "BDF-"+str(m)
 	method = "RadauIIA-"+str(m)
-	sol = model.simulate(T,N,2,method = method)
+	sol = model.simulate(T,N,1,method = method)
 
 	#err[j] = np.abs(sol[0,2]-tau**3)
 	#print(err)
 	err[j] = max(np.abs(sol[0,::m]-ex_sol))
 	#err[j] = np.abs(sol[0,-1])
-#
-#	import matplotlib.pyplot as plt
-##	print(sol)
-#	plt.plot(sol[0,::m])
-#	#plt.plot(sol[1,::m])
-#	plt.plot(ex_sol,linestyle='dashed')
-#	plt.show()
+
+	import matplotlib.pyplot as plt
+#	print(sol)
+	plt.plot(sol[0,::m])
+	#plt.plot(sol[1,::m])
+	plt.plot(ex_sol,linestyle='dashed')
+	plt.show()
 print(err)
 #print("NUMERICAL SOLUTION;")
 #print(sol)
@@ -62,4 +67,4 @@ print(err)
 #plt.loglog(taus,err)
 #plt.loglog(taus,taus**2,linestyle = 'dashed')
 #plt.show()
-	#
+	