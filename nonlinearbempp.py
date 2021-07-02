621e18ab7c9b3391b8c12fe5a2d05d626adef536
Joerg Nick <nick@na.uni-tuebingen.de>
2021-07-02 16:20:01 +0200

    Least square Ansatz for RT interpolation implemented.

--- nonlinearbempp.py
+++ nonlinearbempp.py
@@ -2,28 +2,6 @@ import numpy as np
 import bempp.api
 import inspect
 import matplotlib.pyplot as plt
-dx = 0.3
-grid = bempp.api.shapes.sphere(h=dx)
-RT_space = bempp.api.function_space(grid,"RT",0)
-#def incident_field(x,n,domain_index,result):
-#        #return np.array([np.exp(-50*(x[2]-t+2)**2), 0. * x[2], 0. * x[2]])
-#        result[:] =  np.array([np.exp(-1*(x[2])), 0. * x[2], 0. * x[2]])
-def a(x):
-	return np.linalg.norm(x)**(-0.5)*x
-def tangential_trace(x, n, domain_index, result):
-        result[:] = np.cross(n,np.cross(np.array([np.exp(-1*(x[2])), 0. * x[2], 0. * x[2]]), n))
-def squared_tangtrace(x, n, domain_index, result):
-	trace     = np.cross(n,np.cross(np.array([np.exp(-1*(x[2])), 0. * x[2], 0. * x[2]]), n))
-        result[:] = a(trace)
-#print(np.cross(n,np.cross(np.array([np.exp(-1*(x[2])), 0. * x[2], 0. * x[2]]), n)))
-#@bempp.api.real_callable
-#def curl_trace(x,n,domain_index,result):
-#        curlU=np.array([ 0. * x[2],-100*(x[2]-t+2)*np.exp(-50*(x[2]-t+2)**2), 0. * x[2]])
-#        result[:] = np.cross(curlU , n)
-trace_fun= bempp.api.GridFunction(RT_space, fun=tangential_trace,dual_space=RT_space)
-trace_fun2= bempp.api.GridFunction(RT_space, fun=squared_tangtrace,dual_space=RT_space)
-coefficientsSearched = trace_fun2.coefficients
-coefficientsGiven = trace_fun.coefficients
 def getCoefftoEval(space):
 	dof = space.global_dof_count
 	trace_fun = bempp.api.GridFunction(space, coefficients = np.zeros(dof))
@@ -44,67 +22,116 @@ def getCoefftoEval(space):
 		CoeffToEval[centerdataSize:,j] = np.ravel(evalvertices)
 		coefficients[j] = 0
 	return CoeffToEval
-dof = RT_space.global_dof_count
-CoeffToEval = getCoefftoEval(RT_space)
-trace_fun_given = bempp.api.GridFunction(RT_space, coefficients = coefficientsGiven, dual_space = RT_space)
-centerEvalsGiven = trace_fun_given.evaluate_on_element_centers()
-verticeEvalsGiven = trace_fun_given.evaluate_on_vertices()
-for j in range(len(centerEvalsGiven[0,:])):
-	centerEvalsGiven[:,j] = a(centerEvalsGiven[:,j])
-for j in range(len(verticeEvalsGiven[0,:])):
-	verticeEvalsGiven[:,j] = a(verticeEvalsGiven[:,j])
 
-evalDataFlat = np.concatenate((np.ravel(centerEvalsGiven),np.ravel(verticeEvalsGiven)),axis = 0)
+def squarify(rectMat):
+	dof = len(rectMat[0,:])
+	squareMat = np.zeros((dof,dof))
+	index_sq_mat   = 0
+	index_rect_mat = 0
+	indizes = np.zeros(dof)
+	from scipy.linalg import svdvals
+	while (index_sq_mat < dof):
+		squareMat[index_sq_mat,:] = rectMat[index_rect_mat,:]
+		index_rect_mat = index_rect_mat+1
+		#if (np.linalg.matrix_rank(squareMat[:index_sq_mat+1,:]) == (index_sq_mat+1)):
+		if min(svdvals(squareMat[:index_sq_mat+1,:]))>10**(-3):
+			indizes[index_sq_mat] = index_rect_mat
+			index_sq_mat = index_sq_mat + 1	
+	return squareMat,indizes
 
-#print(np.linalg.norm(CoeffToEval.dot(coefficientsGiven)-evalDataFlat))
-coeffa,res,rank,s = np.linalg.lstsq(CoeffToEval,evalDataFlat)
-print(rank,RT_space.global_dof_count)
-print(res)
-lstsqfun = bempp.api.GridFunction(RT_space, coefficients = coeffa, dual_space = RT_space)
-print((lstsqfun-trace_fun2).l2_norm())
+Am = 8
+errs = np.zeros(Am)
+l2errs = np.zeros(Am)
+dxs = np.linspace(0.1,0.8,Am)
+for it in range(Am):
+	print("Iteration ",it)
+	dx = dxs[it]
+	grid = bempp.api.shapes.sphere(h=dx)
+	RT_space = bempp.api.function_space(grid,"RT",0)
+	#def incident_field(x,n,domain_index,result):
+	#        #return np.array([np.exp(-50*(x[2]-t+2)**2), 0. * x[2], 0. * x[2]])
+	#        result[:] =  np.array([np.exp(-1*(x[2])), 0. * x[2], 0. * x[2]])
+	def a(x):
+		return np.linalg.norm(x)**(-0.5)*x
+	def tangential_trace(x, n, domain_index, result):
+	        result[:] = np.cross(n,np.cross(np.array([np.exp(-1*(x[2])), 0. * x[2], 0. * x[2]]), n))
+	def squared_tangtrace(x, n, domain_index, result):
+		trace     = np.cross(n,np.cross(np.array([np.exp(-1*(x[2])), 0. * x[2], 0. * x[2]]), n))
+	        result[:] = a(trace)
+	#print(np.cross(n,np.cross(np.array([np.exp(-1*(x[2])), 0. * x[2], 0. * x[2]]), n)))
+	#@bempp.api.real_callable
+	#def curl_trace(x,n,domain_index,result):
+	#        curlU=np.array([ 0. * x[2],-100*(x[2]-t+2)*np.exp(-50*(x[2]-t+2)**2), 0. * x[2]])
+	#        result[:] = np.cross(curlU , n)
+	trace_fun= bempp.api.GridFunction(RT_space, fun=tangential_trace,dual_space=RT_space)
+	trace_fun2= bempp.api.GridFunction(RT_space, fun=squared_tangtrace,dual_space=RT_space)
+	coefficientsSearched = trace_fun2.coefficients
+	coefficientsGiven = trace_fun.coefficients
+	dof = RT_space.global_dof_count
+	CoeffToEval = getCoefftoEval(RT_space)
+	trace_fun_given = bempp.api.GridFunction(RT_space, coefficients = coefficientsGiven, dual_space = RT_space)
+	centerEvalsGiven = trace_fun_given.evaluate_on_element_centers()
+	verticeEvalsGiven = trace_fun_given.evaluate_on_vertices()
+	for j in range(len(centerEvalsGiven[0,:])):
+		centerEvalsGiven[:,j] = a(centerEvalsGiven[:,j])
+	for j in range(len(verticeEvalsGiven[0,:])):
+		verticeEvalsGiven[:,j] = a(verticeEvalsGiven[:,j])
+	evalDataFlat = np.concatenate((np.ravel(centerEvalsGiven),np.ravel(verticeEvalsGiven)),axis = 0)
+	#print(np.linalg.norm(CoeffToEval.dot(coefficientsGiven)-evalDataFlat))
+	#coeffa = np.linalg.solve(CoeffToEvalSquare,[evalDataFlat[i] for i in indizes])
+	coeffa,res,rank,s = np.linalg.lstsq(CoeffToEval,evalDataFlat)
+	lstsqfun = bempp.api.GridFunction(RT_space, coefficients = coeffa, dual_space = RT_space)
+	print(coeffa.shape)
+	print(coefficientsSearched.shape)
+	errs[it] = np.max(np.abs(coeffa-coefficientsSearched))
+	l2errs[it] = (lstsqfun-trace_fun2).l2_norm()
+import matplotlib.pyplot as plt
+plt.loglog(dxs,dxs,linestyle='dashed')
+plt.loglog(dxs,errs)
+plt.loglog(dxs,l2errs)
+plt.show()
 #print((np.linalg.norm(coefficientsSearched-coeffa))/np.linalg.norm(coefficientsSearched))
-#print(coeffa)
 #print(np.linalg.norm(CoeffToEval.dot(coeffa)-evalDataFlat))
-#
-#coefficients = np.ones(dof)
-#evalcentersMat = (CoeffToEval.dot(coefficients))
-#
-##print(CoeffToEval.shape)
-##print(np.linalg.matrix_rank(CoeffToEval))
-##print(len(centerdata[0,:]))
-#evalcentersMat = np.reshape(evalcentersMat,(3,len(evalcentersMat)/3))
-#trace_fun= bempp.api.GridFunction(RT_space, coefficients = coefficients)
-#evalcenters = trace_fun.evaluate_on_element_centers()
-#
-##plt.spy(CoeffToEval)
-##plt.show()
-##trace_fun.plot()
-##print(RT_space.global_dof_count)
-##print(trace_fun.evaluate_on_vertices())
-##print(len(coefficients))
-##print(trace_fun.evaluate_on_element_centers())
-##import inspect
-##print(inspect.getsource(bempp.api.GridFunction))
-##def squared_trace(x,n,domain_index,result):
-##	result[:] = trace_fun.evaluate(x,n)
-##print(inspect.getsource(bempp.api.GridFunction))
-##squared_fun= bempp.api.GridFunction(RT_space, fun=squared_trace,dual_space=RT_space)
-##print(dir(trace_fun))
-#
-##from bempp.core.assembly.function_projector import calculate_projection
-##print("TYP : ",type(calculate_projection))
-##print(calculate_projection.__doc__)
-##trace_evals = trace_fun.evaluate_on_vertices()
-##for j in range(len(trace_evals[0,:])):
-##	trace_evals[:,j] = a(trace_evals[:,j])
-##print("##########################################################################")
-#coeffs  = trace_fun.coefficients
-#coeffs2 = trace_fun2.coefficients
-#
-#trace_func_coeff = bempp.api.GridFunction(RT_space, coefficients=coeffs)
-#
-##trace_fun2.plot()
-##curl_fun = bempp.api.GridFunction(RT_space, fun=curl_trace,dual_space=RT_space)
+
+coefficients = np.ones(dof)
+evalcentersMat = (CoeffToEval.dot(coefficients))
+
+#print(CoeffToEval.shape)
+#print(np.linalg.matrix_rank(CoeffToEval))
+#print(len(centerdata[0,:]))
+evalcentersMat = np.reshape(evalcentersMat,(3,len(evalcentersMat)/3))
+trace_fun= bempp.api.GridFunction(RT_space, coefficients = coefficients)
+evalcenters = trace_fun.evaluate_on_element_centers()
+
+#plt.spy(CoeffToEval)
+#plt.show()
+#trace_fun.plot()
+#print(RT_space.global_dof_count)
+#print(trace_fun.evaluate_on_vertices())
+#print(len(coefficients))
+#print(trace_fun.evaluate_on_element_centers())
+#import inspect
+#print(inspect.getsource(bempp.api.GridFunction))
+#def squared_trace(x,n,domain_index,result):
+#	result[:] = trace_fun.evaluate(x,n)
+#print(inspect.getsource(bempp.api.GridFunction))
+#squared_fun= bempp.api.GridFunction(RT_space, fun=squared_trace,dual_space=RT_space)
+#print(dir(trace_fun))
+
+#from bempp.core.assembly.function_projector import calculate_projection
+#print("TYP : ",type(calculate_projection))
+#print(calculate_projection.__doc__)
+#trace_evals = trace_fun.evaluate_on_vertices()
+#for j in range(len(trace_evals[0,:])):
+#	trace_evals[:,j] = a(trace_evals[:,j])
+#print("##########################################################################")
+coeffs  = trace_fun.coefficients
+coeffs2 = trace_fun2.coefficients
+
+trace_func_coeff = bempp.api.GridFunction(RT_space, coefficients=coeffs)
+
+#trace_fun2.plot()
+#curl_fun = bempp.api.GridFunction(RT_space, fun=curl_trace,dual_space=RT_space)
 def harmonic_calderon(s,b,grid):
         points=np.array([[0],[0],[2]])
         #normb=np.linalg.norm(b[0])+np.linalg.norm(b[1])+np.linalg.norm(b[2])
