2dca2bd1323eef24eb83195e63462456fca15a02
Joerg Nick <nick@na.uni-tuebingen.de>
2021-07-13 11:51:01 +0200

    Added multiple grids, generated via distmesh and gmsh.

--- massExperiments.py
+++ massExperiments.py
@@ -1,11 +1,23 @@
 import bempp.api
 from scipy.sparse.linalg import aslinearoperator
-grid = bempp.api.shapes.sphere(h=1)
+#grid = bempp.api.shapes.sphere(h=1)
+#
+mat_contents=scipy.io.loadmat('grids/TorusDOF896.mat')
+Nodes=np.array(mat_contents['Nodes']).T
+rawElements=mat_contents['Elements']
+for j in range(len(rawElements)):
+	betw=rawElements[j][0]
+	rawElements[j][0]=rawElements[j][1]
+	rawElements[j][1]=betw
+Elements=np.array(rawElements).T
+Elements=Elements-1
+grid=bempp.api.grid_from_element_data(Nodes,Elements)
+
 RT_space = bempp.api.function_space(grid,"RT",0)
 identity = bempp.api.operators.boundary.sparse.identity(RT_space, RT_space, RT_space)
 id_weak = identity.weak_form()
 import inspect
 id_sparse = aslinearoperator(id_weak).sparse_operator
-
 id_dense = id_sparse.A
+
 print(id_dense)
