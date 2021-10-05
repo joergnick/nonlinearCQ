New Simulation, corrected sign.

Anim_wave.py
@@ -31,6 +31,6 @@ imobj = ax.imshow( np.zeros((200, 200)), origin='lower', alpha=1.0, zorder=1, as
 
 
 anim = animation.FuncAnimation(fig, animate, init_func=init, repeat = True,
-                               frames=range(0,30), interval=20, blit=True, repeat_delay=500)
-anim.save('NonlinearSphere2.mp4',fps=40,dpi=300,bitrate=-1)
+                               frames=range(0,100), interval=20, blit=True, repeat_delay=500)
+anim.save('NonlinearDonut.mp4',fps=40,dpi=300,bitrate=-1)
 plt.show()

density2pointevals.py
@@ -1,13 +1,30 @@
 import numpy as np
 import bempp.api
 #grid = bempp.api.shapes.sphere(h=2**(0))
-grid = bempp.api.shapes.sphere(h=2**(-0/2))
+import scipy.io
+mat_contents=scipy.io.loadmat("grids/TorusDOF340.mat")
+Nodes=np.array(mat_contents['Nodes']).T
+rawElements=mat_contents['Elements']
+for j in range(len(rawElements)):
+    betw=rawElements[j][0]
+    rawElements[j][0]=rawElements[j][1]
+    rawElements[j][1]=betw
+Elements=np.array(rawElements).T
+Elements=Elements-1
+grid=bempp.api.grid_from_element_data(Nodes,Elements)
+
+
 RT_space = bempp.api.function_space(grid,"RT",0)
 dof = RT_space.global_dof_count
 #sol = np.load('data/solh1.0N64m2.npy')
-sol = np.load('data/solh1.0N30m2.npy')
-m=2
+solDict = np.load('data/donutDOF340.npy').item()
+
+sol = solDict["sol"]
+
 print("Does sol contain Nan values? Answer: "+str(np.isnan(sol).any()))
+m = solDict["m"]
+T = solDict["T"]
+
 N = (len(sol[0,:])-1)/2
 print("N= ",N)
 dof = len(sol[:,0])/2
@@ -40,19 +57,28 @@ points = np.vstack( ( plot_grid[0].ravel()  , 0*np.ones(plot_grid[0].size) , plo
 radius = points[0,:]**2+points[1,:]**2+points[2,:]**2
 def kirchhoff_repr(s,lambda_data):
     print("norm(density)=",np.linalg.norm(lambda_data))
-    if np.linalg.norm(lambda_data)<10**(-3):
+    if (np.linalg.norm(lambda_data)<10**(-3)) or (np.real(s)>50):
         print("Jumped")
         return np.zeros(n_grid_points**2*3)
     phigrid=bempp.api.GridFunction(RT_space,coefficients=lambda_data[0:dof],dual_space=RT_space)
     psigrid=bempp.api.GridFunction(RT_space,coefficients=lambda_data[dof:2*dof],dual_space=RT_space)
-    slp_pot = bempp.api.operators.potential.maxwell.electric_field(RT_space, points, s*1j)
-    dlp_pot = bempp.api.operators.potential.maxwell.magnetic_field(RT_space, points, s*1j)
-    scattered_field_data = -slp_pot * phigrid+dlp_pot*psigrid
+    print("s: ",s)
+    trycounter = 0
+    while trycounter<10:
+        try:
+            slp_pot = bempp.api.operators.potential.maxwell.electric_field(RT_space, points, s*1j)
+            dlp_pot = bempp.api.operators.potential.maxwell.magnetic_field(RT_space, points, s*1j)
+            scattered_field_data = -slp_pot * phigrid+dlp_pot*psigrid
+            break
+        except:
+            print("EXCEPTION CAUGHT, TRYCOUNTER : ",trycounter)
+            trycounter +=1
+
+    print("Is any field nan ? ",np.isnan(scattered_field_data).any())
     scattered_field_data[np.isnan(scattered_field_data)] = 0 
     return scattered_field_data.reshape(n_grid_points**2*3,1)[:,0]
 from linearcq import Conv_Operator
 mSpt_Dpt = Conv_Operator(kirchhoff_repr)
-T=4
 uscatStages = mSpt_Dpt.apply_RKconvol(sol,T,method = "RadauIIA-2")
 uscat = uscatStages[:,::2]
 #uscat = np.zeros((n_grid_points**2*3,N))
@@ -71,13 +97,13 @@ for j in range(N+1):
     #incident_field_data[radius<1]=np.nan
     scat_eval=uscat[:,j].reshape(3,nx*nz)
     #print(scat_eval)
-    field_data = scat_eval + incident_field_data
+    field_data = -scat_eval + incident_field_data
     #field_data = scat_eval 
     #field_data = incident_field_data 
     squared_field_density = np.real(np.sum(field_data * field_data,axis = 0))
     u_ges[:,j]=squared_field_density.T
     #squared_field_density=field_data[2,:]
-    squared_field_density[radius<1]=np.nan
+    #squared_field_density[radius<1]=np.nan
     #squared_field_density[radius<1]=np.nan
     plt.imshow(squared_field_density.reshape((nx, nz)).T,
                cmap='coolwarm', origin='lower',
@@ -90,4 +116,4 @@ for j in range(N+1):
     plt.clim((-1,1))
 
 import scipy.io
-scipy.io.savemat('data/h01N30.mat',dict(u_ges=u_ges,N=N,T=T,plot_grid=plot_grid,points=points))
+scipy.io.savemat('data/DonutFieldDataDOF340.mat',dict(u_ges=u_ges,N=N,T=T,plot_grid=plot_grid,points=points))

nonlinearDonut.py
@@ -52,7 +52,7 @@ def calcRighthandside(c_RK,grid,N,T):
     gTH=IntegralOperator.apply_RKconvol(curls,T,method="RadauIIA-"+str(m),show_progress=False)
     gTH = np.concatenate((np.zeros((dof,1)),gTH),axis = 1)
     #rhs[0:dof,:]=np.real(gTH)-rhs[0:dof,:]
-    return -gTH
+    return gTH
 
 
 def nonlinearScattering(N,gridfilename,T,m):
@@ -110,7 +110,7 @@ def nonlinearScattering(N,gridfilename,T,m):
         def righthandside(self,t,history=None):
             def func_rhs(x,n,domain_index,result):
                 inc =  np.array([np.exp(-50*(x[2]-t+2)**2), 0. * x[2], 0. * x[2]])    
-                tang = np.cross(n,np.cross(inc, n))
+                tang = np.cross(np.cross(inc, n),n)
                 curlU=np.array([ 0. * x[2],-100*(x[2]-t+2)*np.exp(-50*(x[2]-t+2)**2), 0. * x[2]])   
                 result[:] = tang
                 #return np.cross(curlU,n)
@@ -130,19 +130,19 @@ def nonlinearScattering(N,gridfilename,T,m):
     print("GLOBAL DOF: ",dof)
     rhsInhom = calcRighthandside(c_RK,grid,N,T)
     print("Finished RHS.")
-    sol ,counters  = model.simulate(T,N,rhsInhom =rhsInhom, method = "RadauIIA-2",reUse=False)
+    sol ,counters  = model.simulate(T,N,rhsInhom =rhsInhom, method = "RadauIIA-2",reUse=True)
     end = time.time()
     import matplotlib.pyplot as plt
     dof = RT_space.global_dof_count
     norms = [np.linalg.norm(sol[:,k]) for k in range(len(sol[0,:]))]
     return sol
 
-gridfilename='grids/TorusDOF896.mat'
+gridfilename='grids/TorusDOF340.mat'
 m = 3
 N= 100
 T=4
 sol = nonlinearScattering(N,gridfilename,T,m)
-filename = 'data/donutDOF896.npy'
+filename = 'data/donutDOF340N100.npy'
 resDict = dict()
 resDict["sol"] = sol
 resDict["T"] = T